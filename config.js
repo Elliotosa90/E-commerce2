529b5573a3a60b1c902a48b9feef5d7039f991f9
Elliotosa90 <120280217+Elliotosa90@users.noreply.github.com>
2026-07-03 11:24:04 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,13 +6,13 @@
 // ================================================================
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+      apiKey: "AIzaSyDVgP_Ma2TdLRw5luaDod8808L5x8We2Cg",
+    authDomain: "e-commerce-3b0d8.firebaseapp.com",
+    databaseURL: "https://e-commerce-3b0d8-default-rtdb.firebaseio.com",
+    projectId: "e-commerce-3b0d8",
+    storageBucket: "e-commerce-3b0d8.firebasestorage.app",
+    messagingSenderId: "392057419128",
+    appId: "1:392057419128:web:5fed61b34921efc25c048d"
 };
 
 // Initialize Firebase
